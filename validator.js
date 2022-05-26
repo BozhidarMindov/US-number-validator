fa1071ee21e9dd5f71e134eded3d9b8888102b78
Bozhidar Mindov <bbm210@aubg.edu>
2022-05-26 14:39:03 +0300

    first commit

---
+++ validator.js
@@ -0,0 +1,7 @@
+function telephoneCheck(str) {
+    let regex = /^1?\s?(\d{3}|\(\d{3}\))(-?\s?\d{3})(\-?\s?\d{4})$/g;
+  
+    return regex.test(str);
+  }
+  
+  //console.log(telephoneCheck("27576227382"));
